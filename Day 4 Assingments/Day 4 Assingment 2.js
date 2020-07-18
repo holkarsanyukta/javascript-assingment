a468d1fdf51c6f646ecb471012628426f7721bd0
holkarsanyukta <68226998+holkarsanyukta@users.noreply.github.com>
2020-07-18 13:00:07 +0530

    Add files via upload

---
+++ Day 4 Assingments/Day 4 Assingment 2.js
@@ -0,0 +1,9 @@
+const student={
+name:"Helsinki",
+age:24,
+projects:{dice:"two player dice game using Javascript"}
+}
+console.log(name);
+console.log(age);
+console.log(projects);
+console.log(dice);
---
+++ Day 4 Assingments/Day 4 Assingment 3.js
@@ -0,0 +1,2 @@
+let shoppinglist["milk","egg","bread","catfood","fruits","vegetables","maggi","chocolates"};
+console.log(shopinglist[0:8]);
